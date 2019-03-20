420c289a29adf172a96a052b64d1165908863325
Jenhao Chen <jenhaochen@google.com>
2019-03-20 14:57:51 -0700

    raphael: Enable zram writeback
    Bug: 128889899
    Test: Boot to home
    
    Change-Id: I48ae06e54a66a7d6965afbe8db3d2f157cf8a1f4
    Signed-off-by: Ahmed Harhash <Lacentix@tutanota.com>

--- device.mk
+++ device.mk
@@ -505,3 +505,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/sm8150
+
+# ZRAM writeback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
