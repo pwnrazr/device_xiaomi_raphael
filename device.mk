9638ac950a75d7e5fe7dcd18f1d0369faba9c78f
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    raphael: Build and enable fastbootd mode
    Change-Id: I7f4246f3f583dcae7b3bfc0d3ae1cf4d577632cb
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -173,6 +173,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi_raphael \
