7ed93464e94b775d39546e25a033ad40d41f903c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-10-03 11:27:16 +0530

    rapahel: Update perms for hotword blobs
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -238,7 +238,8 @@ PRODUCT_PACKAGES += \
 # Hotword Enrollment
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
-    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
+    $(LOCAL_PATH)/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml
 
 # IFAA manager
 PRODUCT_PACKAGES += \
