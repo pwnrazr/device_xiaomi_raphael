4fc7e8fda2e779bf0fbc7ba4fc32f6931b6a8845
Adhitya Mohan <me@adhityamohan.in>
2020-11-02 22:10:56 +0530

    raphael: Build new aidl vibrator hal

--- msmnile.mk
+++ msmnile.mk
@@ -444,6 +444,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vibrator HAL
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
