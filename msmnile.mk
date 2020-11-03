45505ce924dbcb99cfd04aa5d3520f281a247f26
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-11-03 23:24:03 +0530

    raphael: Build soundtrigger and uprev to 2.2
    This reverts c528ab36d11a2fe7fe97a4b2638a2cd5a3dc6348

--- msmnile.mk
+++ msmnile.mk
@@ -52,8 +52,9 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.soundtrigger@2.2-impl:32 \
     android.hardware.bluetooth.audio@2.0-impl \
 
 PRODUCT_PACKAGES += \
