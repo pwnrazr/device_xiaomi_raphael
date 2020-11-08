3775673004582a7d94e2f4002346d477b6d0f71f
Akash Srivastava <akashniki@gmail.com>
2020-11-08 09:07:14 +0100

    raphael: Don't build 32 bit of sound.effect

--- msmnile.mk
+++ msmnile.mk
@@ -53,8 +53,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@6.0-impl:32 \
-    android.hardware.audio.effect@6.0-impl:32 \
-    android.hardware.soundtrigger@2.2-impl:32 \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
     android.hardware.bluetooth.audio@2.0-impl \
 
 PRODUCT_PACKAGES += \
