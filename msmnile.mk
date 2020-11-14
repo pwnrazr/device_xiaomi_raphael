4855a0abb80dba9e108ca0d489024fc41826f6ff
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-11-14 14:14:47 +0500

    raphael: display: Build libdisplayconfig.qti
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -138,7 +138,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
     libcodec2_hidl@1.0.vendor \
-    libavservices_minijail_vendor
+    libavservices_minijail_vendor \
+    libavservices_minijail_32 \
+    libavservices_minijail.vendor \
 
 # Display
 PRODUCT_PACKAGES += \
@@ -493,8 +495,8 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libdisplayconfig \
-    libdisplayconfig.vendor \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
     libnl \
     libqdMetaData \
     libqdMetaData.system
