53dbed2e1241e3edd3b400cdc49e8321be1e7956
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2021-03-24 09:36:37 +0200

    raphael: ant: Build ANT+ HAL from source
    * Add HAL support in mainfest

--- device.mk
+++ device.mk
@@ -49,6 +49,12 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService-Soong \
+    com.dsi.ant.antradio_library \
+    com.dsi.ant@1.0.vendor
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
