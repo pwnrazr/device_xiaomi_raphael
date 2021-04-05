b89092c816f1a623c63122ed2110ad3add916a02
Daniel Rosenberg <drosen@google.com>
2021-04-05 16:27:51 +0530

    raphael: Enable Project ID Quota support on userdata.
     * These features enable wide inodes on userdata, which
       is required for project ID quotas. Wide inodes can
       only be set at MKFS time.
    
     * This also enables casefolding, which is required for the
       removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67
    Signed-off-by: Ahmed Harhash <Lacentix@tutanota.com>

--- device.mk
+++ device.mk
@@ -28,6 +28,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/raphael/raphael-vendor.mk)
 
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
