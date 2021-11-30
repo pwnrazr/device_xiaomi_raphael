0730428e8386e12e8ea7d197c8878535bf88d7e3
Ahmed Harhash <Lacentix@tutanota.com>
2021-12-01 01:39:22 +0200

    raphael: Add vibrator service HAL
    Signed-off-by: Ahmed Harhash <Lacentix@tutanota.com>

--- device.mk
+++ device.mk
@@ -471,7 +471,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator HAL
-$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
 
 # Vulkan
 PRODUCT_PACKAGES += \
