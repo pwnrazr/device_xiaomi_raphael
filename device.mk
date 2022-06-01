60f3a956ffadcf6c14b9cb358b58633bcb4322de
zclkkk <2484392714@qq.com>
2022-06-01 18:33:52 +0800

    raphael: Import thermal configs
    Co-authored-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: Ic00d036e57d463f4a07e54ca193251c1f3ac63f3

--- device.mk
+++ device.mk
@@ -468,6 +468,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.xiaomi \
     thermal_symlinks
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/thermal/etc/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
+    $(LOCAL_PATH)/thermal/etc/thermal-engine-msmnile-normal_mode.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-msmnile-normal_mode.conf
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
