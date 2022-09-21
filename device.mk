3c14d16a3f8a22fd25a3fe35da628744bf6ecd80
Bruno Martins <bgcngm@gmail.com>
2022-09-22 00:40:01 +0100

    raphael: Drop component overrides
    This is no longer needed as of Android 13.
    
    Change-Id: I38435c47c8781678f82014decbbae40dcab0a07b

--- device.mk
+++ device.mk
@@ -129,11 +129,6 @@ PRODUCT_PACKAGES += \
     libavservices_minijail_32 \
     libavservices_minijail.vendor 
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
-    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
