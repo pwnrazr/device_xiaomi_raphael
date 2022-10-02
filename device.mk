b259912195b1a7fe2399e83421cee9c7665a2305
johnmart19 <johnivan19999@gmail.com>
2022-10-02 19:27:39 +0300

    raphael: Audio: Build android.hardware.audio@7.1-util
    - Matching stock environment
    - Also provies compilation of:
    * android.hardware.audio@7.1
    * android.hardware.audio.common@7.0
    * android.hardware.audio.common@7.1-enums
    * android.hardware.audio.common@7.1-util
    
    Currently in system/lib we had only android.hardware.audio.common@2.0 & @5.0
    In system/lib64 we had only android.hardware.audio.common@2.0.so @4.0 @4.0-util @5.0 @5.0-util @6.0 @6.0-util and AIDL

--- device.mk
+++ device.mk
@@ -58,6 +58,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@7.1-impl \
+    android.hardware.audio@7.1-util \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     android.hardware.bluetooth.audio@2.0 \
