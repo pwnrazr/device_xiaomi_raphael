54286b9dace50c6fab99de5d0859c054f4f4838d
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2022-10-29 00:08:04 +0800

    Revert "Revert "raphael: Build KProfiles""
    After retrying again with different pelt halflife I've determined that I suck at testing these things lol
    Bring back KProfiles
    
    This reverts commit 2453b8d82cafdc38fe6a83c8c9de16cea8bdcaa5.

--- device.mk
+++ device.mk
@@ -298,6 +298,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # Media
 PRODUCT_PACKAGES += \
     libarbitrarybytes \
