b748e42b24910b8126bfea7c94e24b1845de42af
someone5678 <nemui3353@gmail.com>
2022-11-07 13:59:56 +0900

    raphael: bt: Rename libbthost_if to libbthost_if_sink
    * Also add libprocessgroup into LOCAL_SHARED_LIBRARIES
    
    Ref:
    https://github.com/LineageOS/android_device_razer_aura/commit/f63109de7a63ef508c9b7f5aedae076052fd4089

--- bthost_ipc/Android.mk
+++ bthost_ipc/Android.mk
@@ -14,10 +14,10 @@ LOCAL_SRC_FILES := bthost_ipc.c
 LOCAL_C_INCLUDES += \
         $(LOCAL_PATH)/bthost_ipc.h
 
-LOCAL_MODULE := libbthost_if
+LOCAL_MODULE := libbthost_if_sink
 LOCAL_MODULE_SUFFIX  := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_SHARED_LIBRARIES := liblog libcutils libprocessgroup
 LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_MODULE_TAGS := optional
 ifdef TARGET_2ND_ARCH
