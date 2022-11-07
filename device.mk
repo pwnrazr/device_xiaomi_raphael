7e84c4ea6803b82426128b8051b81b649b08453c
someone5678 <nemui3353@gmail.com>
2022-11-07 23:48:06 +0900

    raphael: bt: Build libbthost_if_sink
    * We imported libbthost_if and renamed it to libbthost_if_sink

--- device.mk
+++ device.mk
@@ -105,8 +105,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
-    
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    libbthost_if_sink
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
