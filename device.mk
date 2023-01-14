a91fff4d877141c0b2204053f832615c513967cd
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-01-14 13:29:04 +0800

    Build sensors.raphael_udfps
    Referenced:
    https://github.com/yaap/device_oneplus_sm8150-common/commit/22f85f64ec7578bcb07c409a53eb8294c69e3d67

--- device.mk
+++ device.mk
@@ -429,7 +429,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    sensors.raphael_udfps
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
