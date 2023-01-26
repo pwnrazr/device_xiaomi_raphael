8070bfdb72a4abe55eca9dae5d822f68de19c97e
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-01-26 20:17:54 +0800

    TEST build sensors.xiaomi

--- device.mk
+++ device.mk
@@ -430,8 +430,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
-    sensors.udfps \
-    sensors.double_tap
+    sensors.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
