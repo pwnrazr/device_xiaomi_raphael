04d463491abbda813652bfacdc58ec34a1549f52
John Galt <johngaltfirstrun@gmail.com>
2023-02-22 12:59:43 -0500

    raphael: utilize task_profiles.json + cgroups.json
    We have full kernel support for uclamp and all additional functionality
    provided on current profiles.

--- device.mk
+++ device.mk
@@ -452,6 +452,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libwatermark_shim
 
+# Task Profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
