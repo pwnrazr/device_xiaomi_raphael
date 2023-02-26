5b4286baa8bbbc5c1378d2c20192b8ded76febbb
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-02-26 12:06:37 +0800

    raphael: build some camera stuff
    From: https://github.com/xiaomi-sm8150-devs/android_device_xiaomi_sm8150-common/blob/41ba71f8aa1cd368d4f904ae56124ee47a14bba6/msmnile.mk#L117

--- device.mk
+++ device.mk
@@ -118,7 +118,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    libgui_vendor \
+    libstdc++.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
