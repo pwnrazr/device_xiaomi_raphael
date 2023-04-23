4e0445782e5ade7f9c3f4015bbd307e11bb441b3
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-04-23 14:43:52 +0800

    Revert "raphael: sensors: import 1.0 sensors impl from https://github.com/LineageOS/android_hardware_xiaomi/tree/3d946592e6336aa8b9ed2da0156011a9c964637f/hidl/sensors/1.0"
    I'll do it a little more properly
    
    This reverts commit b3ee3b636b576ba928fd1e71f0972cc9789a3f2b.

--- device.mk
+++ device.mk
@@ -429,7 +429,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl-raphael \
+    android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@2.0 \
     android.hardware.sensors@2.1 \
