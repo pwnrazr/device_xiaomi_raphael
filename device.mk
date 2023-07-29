298af93d726cbf3a06adb54af420b7528a4be286
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-07-29 14:09:58 +0800

    raphael: actually build libqti-perfd-client
    I missed this whoops
    
    Fixes bffb7388f

--- device.mk
+++ device.mk
@@ -392,6 +392,10 @@ PRODUCT_COPY_FILES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
