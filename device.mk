12d1ad25f659763624fbba9cbde18820ffd50781
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-08-02 14:54:47 +0800

    raphael: Override sensors@1.0-service
    add input and move to task_profiles

--- device.mk
+++ device.mk
@@ -395,7 +395,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Vendor init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/services/android.hardware.drm@1.3-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.3-service.widevine.rc \
-    $(LOCAL_PATH)/configs/services/android.hardware.neuralnetworks@1.3-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc
+    $(LOCAL_PATH)/configs/services/android.hardware.neuralnetworks@1.3-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc \
+    $(LOCAL_PATH)/configs/services/android.hardware.sensors@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.sensors@1.0-service.rc
 
 # Perf
 PRODUCT_PACKAGES += \
