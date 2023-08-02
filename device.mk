f798ed95f0830e0179bce3b1ba8b7267faa3ced5
RealJohnGalt <johngaltfirstrun@gmail.com>
2023-08-02 14:51:43 +0800

    raphael: vendor/etc/init: move two services to DT
    For task_profiles migration

--- device.mk
+++ device.mk
@@ -392,6 +392,11 @@ PRODUCT_COPY_FILES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Vendor init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/services/android.hardware.drm@1.3-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.3-service.widevine.rc \
+    $(LOCAL_PATH)/configs/services/android.hardware.neuralnetworks@1.3-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc
+
 # Perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
