46e965a91b3329b12c364c08f2d457126316a608
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-10-29 09:24:34 +0800

    Revert "raphael: Audio: Build android.hardware.audio@7.1-util"
    This reverts commit b259912195b1a7fe2399e83421cee9c7665a2305.

--- device.mk
+++ device.mk
@@ -69,7 +69,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@7.1-impl \
-    android.hardware.audio@7.1-util \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
